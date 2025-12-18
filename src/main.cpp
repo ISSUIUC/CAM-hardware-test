f8253fefe43bbbc206adbcfc6698533908bad127
Michael K <mkarpov2@illinois.edu>
2025-12-17 20:33:13 -0600

    ignore the error

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,18 @@
 #include <Arduino.h>
 #include <pins.h>
 
-const int LED_PIN = 51;
+const int LED_PIN = 23;
 
 void setup()
 {
     pinMode(LED_PIN, OUTPUT);
     digitalWrite(LED_PIN, LOW);
+    Serial.begin(115200);
 }
 
 void loop()
 {
+    Serial.println("HIII");
     digitalWrite(LED_PIN, HIGH);
     delay(500);
     digitalWrite(LED_PIN, LOW);
