edb0e567833a254d22bfda50d936bba3775152b9
rndas27 <143290397+rndas27@users.noreply.github.com>
2025-12-30 00:38:31 -0500

    Added SW Encoder Support

--- lib/tvp5151/venc.cpp
+++ lib/tvp5151/venc.cpp
@@ -7,8 +7,8 @@
 #include "esp_h264_alloc.h"
 #include "../../components/esp_h264/test_apps/main/h264_io.h"
 
-esp_h264_enc_cfg_hw_t H264::set_hw_H264_config_single(esp_h264_raw_format_t format, uint8_t fps, uint16_t height, uint16_t width, uint32_t bitrate, uint8_t qp_min, uint8_t qp_max, uint8_t gop) {
-    esp_h264_enc_cfg_hw_t config;
+esp_h264_enc_cfg_t H264::set_H264_config_single(esp_h264_raw_format_t format, uint8_t fps, uint16_t height, uint16_t width, uint32_t bitrate, uint8_t qp_min, uint8_t qp_max, uint8_t gop) {
+    esp_h264_enc_cfg_t config;
 
     config.pic_type = format;
     config.gop = gop;
@@ -19,24 +19,6 @@ esp_h264_enc_cfg_hw_t H264::set_hw_H264_config_single(esp_h264_raw_format_t form
     return config;
 }
 
-// esp_h264_enc_cfg_dual_hw_t H264::set_hw_H264_config_dual(esp_h264_raw_format_t format, uint8_t fps, uint16_t height, uint16_t width, uint32_t bitrate, uint8_t qp_min, uint8_t qp_max, uint8_t gop) {
-//     esp_h264_enc_cfg_dual_hw_t config;
-
-//     config.cfg0.pic_type = format;
-//     config.cfg0.gop = gop;
-//     config.cfg0.fps = fps;
-//     config.cfg0.rc = (esp_h264_enc_rc_t) {bitrate, qp_min, qp_max};
-//     config.cfg0.res = (esp_h264_resolution_t) {width, height};
-
-//     config.cfg1.pic_type = format;
-//     config.cfg1.gop = gop;
-//     config.cfg1.fps = fps;
-//     config.cfg1.rc = (esp_h264_enc_rc_t) {bitrate, qp_min, qp_max};
-//     config.cfg1.res = (esp_h264_resolution_t) {width, height};
-
-//     return config;
-// }
-
 esp_h264_enc_in_frame_t* H264::get_inframe() {
     return &in_frame;
 }
@@ -46,10 +28,10 @@ esp_h264_enc_out_frame_t* H264::get_ouyframe() {
 }
 
 //adapted from esp_h264_hw_enc_test.c:19, change calloc function calls
-esp_h264_err_t H264::init_hw_H264_enc_single(esp_h264_enc_cfg_hw_t config) {
+esp_h264_err_t H264::init_H264_enc_single(esp_h264_enc_cfg_t config, enc_type type) {
     cfg = config;
     esp_h264_err_t ret = ESP_H264_ERR_OK;
-    uint16_t width = ((cfg.res.width + 15) >> 4 << 4);
+    uint16_t width = ((cfg.res.width + 15) >> 4 << 4);      //possibly remove (only in some of example code)
     uint16_t height = ((cfg.res.height + 15) >> 4 << 4);
 
     in_frame.raw_data.len = (int)((float)width * height * ESP_H264_GET_BPP_BY_PIC_TYPE(cfg.pic_type));
@@ -66,11 +48,16 @@ esp_h264_err_t H264::init_hw_H264_enc_single(esp_h264_enc_cfg_hw_t config) {
         return ESP_H264_ERR_MEM;
     }
 
-    ret = esp_h264_enc_hw_new(&cfg, &enc);  //has internal memory allocation
+    if(type == HW) {
+        ret = esp_h264_enc_hw_new(&cfg, &enc);  //has internal memory allocation
+    } else {
+        ret = esp_h264_enc_sw_new(&cfg, &enc);  //has internal memory allocation
+    }
     if (ret != ESP_H264_ERR_OK) {
         printf("new failed. line %d \n", __LINE__);
         return ret;
     }
+    
     ret = esp_h264_enc_open(enc);    //has internal memory allocation
     if (ret != ESP_H264_ERR_OK) {
         printf("open failed .line %d \n", __LINE__);
@@ -80,7 +67,7 @@ esp_h264_err_t H264::init_hw_H264_enc_single(esp_h264_enc_cfg_hw_t config) {
     return ret;
 }
 
-esp_h264_err_t H264::run_hw_H264_enc_single() {
+esp_h264_err_t H264::run_H264_enc_single() {
     esp_h264_err_t ret = ESP_H264_ERR_OK;
     
     //need to figure out what read_enc_cb and write_enc_cb do
@@ -100,7 +87,7 @@ esp_h264_err_t H264::run_hw_H264_enc_single() {
     return ret;
 }
 
-esp_h264_err_t H264::close_hw_H264_enc_single() {
+esp_h264_err_t H264::close_H264_enc_single() {
     esp_h264_err_t ret = ESP_H264_ERR_OK;
     ret = esp_h264_enc_close(enc);
     ret = esp_h264_enc_del(enc);
